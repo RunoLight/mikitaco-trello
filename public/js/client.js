435dacd8d50fd775e57bdc794928358642a73c03
Runo <31756050+RunoLight@users.noreply.github.com>
2021-01-25 16:20:13 +1000

    1

--- public/js/client.js
+++ public/js/client.js
@@ -5,7 +5,12 @@ var Promise = TrelloPowerUp.Promise;
 var BLACK_ROCKET_ICON =
   "https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421";
 
+
+
 TrelloPowerUp.initialize({
+
+
+
   // Start adding handlers for your capabilities here!
   'board-buttons': function (t, opts) {
     return [{
@@ -17,7 +22,7 @@ TrelloPowerUp.initialize({
       callback: onBtnClick,
 
     }];
-  };
+  },
 
   "card-buttons": function(t, options) {
     return t.set("member", "shared", "hello", "world").then(function() {
