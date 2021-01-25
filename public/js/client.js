6a532402b6424052a7d04e040b938f3d7d9eea31
Runo <31756050+RunoLight@users.noreply.github.com>
2021-01-26 00:53:18 +1000

    1

--- public/js/client.js
+++ public/js/client.js
@@ -1,99 +1,43 @@
 /* global TrelloPowerUp */
 
-// we can access Bluebird Promises as follows
-window.Promise = TrelloPowerUp.Promise;
+var Promise = TrelloPowerUp.Promise;
 
-// helper function to fetch data and generate card badges
-var getBadge = function(t, detail) {
-  return t.getAll()
-  .then(function(storedData){
-    if (!storedData || !storedData.card || !storedData.card.shared) {
-      return [];
-    }
-    
-    var data = storedData.card.shared;
-    if (!data.unixTime || new Date(data.unixTime * 1000) < Date.now()) {
-      return [];
-    }
-    
-    return t.card('id', 'closed')
-    .then(function(card){
-      if (data.idCard !== card.id || !card.closed) {
-        $.post('/snooze/' + card.id + '/verify', function(){
-          t.set('card', 'shared', { idCard: null, time: null, unixTime: null });
-        });
-        return [];
-      }
-      var badge = {
-        icon: 'https://cdn.hyperdev.com/07656aca-9ccd-4ad1-823c-dbd039f7fccc%2Fzzz-grey.svg'
-      };
-      if (detail) {
-        badge.title = 'Card Snoozed Until';
-        badge.text = data.time;
-      }
-      return [badge];
-    })
-    .catch(function(){
-      return [];
-    });
-  })
-  .catch(function(){
-    return [];
-  });
-};
+var BLACK_ROCKET_ICON =
+  "https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421";
 
-// We need to call initialize to get all of our capability handles set up and registered with Trello
+
+
 TrelloPowerUp.initialize({
-  'card-badges': function(t){
-    return getBadge(t, false);
+
+
+
+  // Start adding handlers for your capabilities here!
+  'board-buttons': function (t, opts) {
+    return [{
+      icon: {
+        dark: WHITE_ICON,
+        light: BLACK_ICON
+      },
+      text: 'Callback',
+      callback: onBtnClick,
+
+    }];
   },
-  'card-buttons': function(t, opts) {
-    // check that viewing member has write permissions on this board
-    if (opts.context.permissions.board !== 'write') {
-      return [];
-    }
-    return t.get('member', 'private', 'token')
-    .then(function(token){
-      return [{
-        icon: 'https://cdn.hyperdev.com/07656aca-9ccd-4ad1-823c-dbd039f7fccc%2Fzzz-grey.svg',
-        text: 'Snooze',
-        callback: function(context) {
-          if (!token) {
-            context.popup({
-              title: 'Authorize Your Account',
-              url: './auth.html',
-              height: 75
-            });
-          } else {
-            return context.popup({
-              title: 'Change Snooze Time',
-              url: './set-snooze.html',
-              height: 411
+
+  "card-buttons": function(t, options) {
+    return t.set("member", "shared", "hello", "world").then(function() {
+      return [
+        {
+          icon: BLACK_ROCKET_ICON,
+          text: "Estimate Size",
+          callback: function(t) {
+            return t.popup({
+              title: "Estimation",
+              url: "estimate.html"
             });
           }
         }
-      }];
-    });
-  },
-  'card-detail-badges': function(t, opts){
-    var editable = opts.context.permissions.board === 'write';
-    var clickCallback = function(context){
-      return context.popup({
-        title: 'Change Snooze Time',
-        url: './set-snooze.html',
-        height: 411 // initial height of popup window
-      });
-    };
-    return getBadge(t, true)
-    .then(function(badges){
-      if (badges && badges.length === 1 && editable) {
-        // add callback if editable
-        badges[0].callback = clickCallback;
-      }
-      return badges;
-    })
-    .catch(function(){
-      return [];
+      ];
     });
   }
 });
