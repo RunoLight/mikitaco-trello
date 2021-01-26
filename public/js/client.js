505bb5825225ac82c8326db4d0798cc6ec913a06
Runo <31756050+RunoLight@users.noreply.github.com>
2021-01-26 13:18:27 +1000

    1

--- public/js/client.js
+++ public/js/client.js
@@ -2,44 +2,23 @@
 
 var Promise = TrelloPowerUp.Promise;
 
-var BLACK_ROCKET_ICON =
-  "https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421";
-
-
+var BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421';
 
 TrelloPowerUp.initialize({
-
-
-
   // Start adding handlers for your capabilities here!
-  // 'board-buttons': function (t, opts) {
-  //   return [{
-  //     icon: {
-  //       dark: BLACK_ROCKET_ICON,
-  //       light: BLACK_ROCKET_ICON
-  //     },
-  //     text: 'Callback',
-  //     callback: function () {
-  //       console.log("sex");
-  //     },
-  //
-  //   }];
-  // },
-
-  "card-buttons": function(t, options) {
-    return t.set("member", "shared", "hello", "world").then(function() {
-      return [
-        {
-          icon: BLACK_ROCKET_ICON,
-          text: "Estimate Size 2",
-          callback: function(t) {
-            return t.popup({
-              title: "Estimation",
-              url: "estimate.html"
-            });
-          }
-        }
-      ];
-    });
-  }
+	 'card-buttons': function(t, options) {
+	 return t.set("member", "shared", "hello", "world")
+	 .then(function(){
+	 	  return [{
+	 icon: BLACK_ROCKET_ICON,
+	 		  text: 'Estimate Size',
+	       callback: function(t) {
+	         return t.popup({
+	           title: "Estimation",
+	           url: 'estimate.html',
+	         });
+	       }
+	 	  }];
+	 })
+	 },
 });
