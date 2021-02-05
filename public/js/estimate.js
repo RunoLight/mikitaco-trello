d7388cccd4d7740ceceb897925704275dbfc2804
Runo <31756050+RunoLight@users.noreply.github.com>
2021-02-06 01:08:39 +1000

    1

--- public/js/estimate.js
+++ public/js/estimate.js
@@ -4,7 +4,7 @@ var t = TrelloPowerUp.iframe();
 
 window.estimate.addEventListener('submit', function(event){
     event.preventDefault();
-    return t.set('card', 'shared', 'status', window.estimateSize.value)
+    return t.set('card', 'shared', 'status', window.status.value)
         .then(function(){
             t.closePopup();
         });
@@ -12,8 +12,8 @@ window.estimate.addEventListener('submit', function(event){
 
 t.render(function(){
     return t.get('card', 'shared', 'status')
-        .then(function(estimate){
-            window.estimateSize.value = estimate;
+        .then(function(status){
+            window.status.value = status;
         })
         .then(function(){
             t.sizeTo('#content').done();
