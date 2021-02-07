4be0a0669e17a6d485768773ce1c9463eb51f0fc
Runo <31756050+RunoLight@users.noreply.github.com>
2021-02-07 12:49:17 +1000

    change card name on status change

--- public/js/status.js
+++ public/js/status.js
@@ -1,14 +1,73 @@
 /* global TrelloPowerUp */
+function parseCardDesc(desc) {
 
-var t = TrelloPowerUp.iframe();
+}
+
+const fetch = require('node-fetch');
+
+var statusToId = {
+    "inbox" : 0,
+    "cooking" : 1,
+    "to-deliver" : 2,
+    "on-the-way" : 3,
+    "delivered" : 4,
+    "canceled" : 5
+}
+
+var opts = {
+    appKey: 'fff4efd3f3ba3f0f515bd2aa84e97cd8',
+    appName: 'Mikitaco business power up'
+}
+
+var t = TrelloPowerUp.iframe(opts);
 
 window.status_refresh.addEventListener('submit', function(event){
     event.preventDefault();
 
     t.sizeTo('#status_form');
+    var selectedStatus = window.selected_status.value;
 
-    return t.set('card', 'shared', 'status', window.selected_status.value)
+    return t.set('card', 'shared', 'status', selectedStatus)
         .then(function(){
+            t.getRestApi()
+                .getToken()
+                .then(function (token) {
+                    if (!token) {
+                        t.popup({
+                            title: 'Authorize to continue',
+                            url: './restAuthorize.html'
+                        })
+                    }
+
+                    var thisCard = t.card("all");
+                    var neededListNum = statusToId[selectedStatus];
+                    var newListId = t.lists("id")[neededListNum].id;
+                    var currentListId = t.list("id");
+                    // add twice then?
+                    console.log(`list id: current ${currentListId}, new ${newListId}`);
+
+                    var url = `https://api.trello.com/1/cards/${thisCard.id}?`;
+                    var bodyParams = {
+                        key: opts.appKey,
+                        token: token,
+                        name: "miki ta co :)"
+                    }
+
+                    fetch(url, {
+                        method: 'PUT',
+                        headers: {
+                            'Accept': 'application/json'
+                        },
+                        body: bodyParams
+                    })
+                        .then(response => {
+                            console.log(`Response: ${response.status} ${response.statusText}`);
+                            return response.text();
+                        })
+                        .then(text => console.log(text))
+                        .then(err => console.error(err));
+                }
+            );
             t.closePopup();
         });
 });
